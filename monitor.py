3063f0c0e72d864c0e5e721c702ae423ba43c6e0
Setin2 <stefancernat88@yahoo.com>
2023-03-07 17:18:02 +0100

    saving data

--- monitor.py
+++ monitor.py
@@ -7,36 +7,51 @@ from kafka import KafkaConsumer
 class Plot():
     def __init__(self):
         self.labels = ['Used', 'Available']
-        self.fig1, self.ax1 = plt.subplots(1, 2)
+        self.fig1, self.ax1 = plt.subplots(2, 2)
         plt.ion()
-        self.ax1[0].pie([50, 50], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
-        self.ax1[0].axis('equal')
-        self.ax1[1].pie([50, 50], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
-        self.ax1[1].axis('equal')
+        self.ax1[0, 0].pie([50, 50], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+        self.ax1[0, 0].axis('equal')
+        self.ax1[0, 0].title.set_text('CPU')
+        self.ax1[0, 1].pie([50, 50], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+        self.ax1[0, 1].axis('equal')
+        self.ax1[0, 1].title.set_text('RAM')
+        self.ax1[1, 0].pie([50, 50], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+        self.ax1[1, 0].axis('equal')
+        self.ax1[1, 0].title.set_text('DISK')
         plt.pause(0.001)
         plt.show()
 
-    def update(self, val, resource):
-        if resource == "CPU":
-            self.ax1[0].clear()
-            self.ax1[0].pie([val, 100-val], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
-        elif resource == "RAM":
-            self.ax1[1].clear()
-            self.ax1[1].pie([val, 100-val], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+    def update(self, key, value):
+        if "CPU" in key:
+            self.ax1[0, 0].clear()
+            self.ax1[0, 0].pie([value, 100-value], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+            self.ax1[0, 0].title.set_text('CPU')
+        elif "RAM" in key:
+            self.ax1[0, 1].clear()
+            self.ax1[0, 1].pie([value, 100-value], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+            self.ax1[0, 1].title.set_text('RAM')
+        elif "DISK" in key:
+            self.ax1[1, 0].clear()
+            self.ax1[1, 0].pie([value, 100-value], explode=(0.1, 0), labels=self.labels, autopct='%1.1f%%', startangle=90)
+            self.ax1[1, 0].title.set_text('DISK')
         # re-drawing the figure
         self.fig1.canvas.draw()
         # to flush the GUI events
         self.fig1.canvas.flush_events()
 
-consumer = KafkaConsumer("my-topic", "my-group", bootstrap_servers=['localhost:9092'])
+def save_to_file(filename, data):
+    with open('data/{fname}'.format(fname = filename), 'a') as f:
+        f.write(data + "\n")
+
+consumer = KafkaConsumer("resources", "my-group", bootstrap_servers=['localhost:9092'])
 plot = Plot()
 
 for message in consumer:
     key = message.key.decode("utf-8")
     value = message.value.decode("utf-8")
     
-    #plot.update(float(value), key)
-    plot.update(float(value), key)
+    plot.update(key, float(value))
+    save_to_file(key, value)
 
     # consume earliest available messages, don't commit offsets
     KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
--- resource-monitor/producer.py
+++ resource-monitor/producer.py
@@ -1,5 +1,13 @@
+import sys
+import psutil
+import time
+import producer
+import datetime
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
+
+KAFKA_TOPIC = 'resource-monitor'
+KAFKA_BOOTSTRAP_SERVERS = 'kafka-broker:9092'
 
 class Producer:
     def __init__(self):
@@ -23,3 +31,41 @@ class Producer:
 
         # block until all async messages are sent
         self.producer.flush()
+
+def get_cpu_usage():
+    return psutil.cpu_percent()
+
+# RAM 
+def get_memory_usage():
+    mem_stats = psutil.virtual_memory()
+    mem_total = mem_stats.total
+    mem_used = mem_stats.used
+    mem_percent = mem_stats.percent
+    return mem_percent
+
+def get_disk_used():
+    disk_stats = psutil.disk_usage('/')
+    disk_total = disk_stats.total
+    disk_used = disk_stats.used
+    disk_percent = disk_stats.percent
+    return disk_percent
+
+def get_current_time():
+    now = datetime.datetime.now()
+    return now.time()
+
+if __name__ == '__main__':
+    #producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    producer = Producer()
+    service = sys.argv[1]
+
+    while True:
+        cpu_usage = get_cpu_usage()
+        mem_usage = get_memory_usage()
+        disk_usage = get_disk_used()
+        producer.send("resources", service + " CPU", str(cpu_usage))
+        producer.send("resources", service + " RAM", str(mem_usage))
+        producer.send("resources", service + " DISK", str(disk_usage))
+
+        # Wait for 5 seconds before collecting and sending next data point
+        time.sleep(1)
